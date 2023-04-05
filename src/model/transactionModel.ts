39ce46ff207dd38b64c50c28391b6f523f01a1f4
Farooq Ajibade <timiajibade24@gmail.com>
2023-04-05 15:06:39 +0100

    added timestamp to transaction model

--- src/model/transactionModel.ts
+++ src/model/transactionModel.ts
@@ -1,15 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema({
-  userId: { type: String, required: true },
-  status: { type: String },
-  amount: { type: Number, required: true },
-  transactionType: { type: String, required: true },
-  processed: {type: Boolean, default: false},
-  ref: {type: String,}
-});
+const transactionSchema = new Schema(
+  {
+    userId: { type: String, required: true },
+    status: { type: String },
+    amount: { type: Number, required: true },
+    transactionType: { type: String, required: true },
+    processed: { type: Boolean, default: false },
+    ref: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Transaction = mongoose.model("transaction", transactionSchema);
+const Transaction = mongoose.model('transaction', transactionSchema);
 export default Transaction;
