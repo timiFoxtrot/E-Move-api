151829031ec37226c7fa16ec77a2498fd6f85d6a
Decagon <decagon@Decagons-MacBook-Air-2.local>
2023-05-13 22:36:00 +0100

    Added cors options

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import cors from "cors"
+import cors from 'cors';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -18,7 +18,12 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:3001',
+    credentials: true, //access-control-allow-credentials:true
+  })
+);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,38 +31,38 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the E-Move Api')
-})
+  res.send('Welcome to the E-Move Api');
+});
 
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (
-    err: HttpError,
-    req: Request,
-    res: Response,
-    next: NextFunction
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 const port = process.env.PORT || 8081;
 
 app.listen(port, async () => {
-    console.log(`App is running at http://localhost:${port}`);
-    await connect();
+  console.log(`App is running at http://localhost:${port}`);
+  await connect();
 });
 
 export default app;
